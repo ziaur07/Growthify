47e814be9911477afd35b836d6c7e1f8dd3e3711
ziaur07 <zrzbusiness07@gmail.com>
2024-03-01 16:48:08 +0600

    prototype_done

--- SMMA/static/SMMA/script.js
+++ SMMA/static/SMMA/script.js
@@ -1,9 +1,25 @@
+// slider
+
 document.addEventListener('DOMContentLoaded', function() {
     $('.testimonial-carousel').slick({
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         adaptiveHeight: true
+        
+    });
+});
+
+
+// FAQs
+
+document.addEventListener('DOMContentLoaded', function() {
+    var faqQuestions = document.querySelectorAll('.faq-question');
+    faqQuestions.forEach(function(question) {
+        question.addEventListener('click', function() {
+            var answer = this.nextElementSibling;
+            answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+        });
     });
 });
--- SMMA/static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $('.testimonial-carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
-});
